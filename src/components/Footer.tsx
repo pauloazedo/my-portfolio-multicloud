4f9dfd0b847e135ec9ad839b1929247c89387e62
pauloazedo <paulo.azedo@gmail.com>
2025-04-02 21:11:28 -0500

    Initial clean state for future releases

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer className="w-full text-center py-4 border-t border-blue-800 bg-zinc-900 text-zinc-400 text-sm">
-        © 2025 Paulo Azedo. All rights reserved.
-      </footer>
-    )
-  }
-  
-  export default Footer  
