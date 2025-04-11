3977b25650ace891f5d8690e4429ad8194af80e3
pauloazedo <paulo.azedo@gmail.com>
2025-04-11 13:24:52 -0500

    Testing pipeline

--- my-portfolio/frontend/src/components/Footer.tsx
+++ my-portfolio/frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="w-full text-center py-4 border-t border-blue-800 bg-zinc-900 text-zinc-400 text-sm">
-        © 2025 Paulo Azedo. All rights reserved v0.0.17 .
+        © 2025 Paulo Azedo. All rights reserved v0.0.17  .
       </footer>
     )
   }
