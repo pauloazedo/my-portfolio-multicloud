eae0f9b507f6117cc41824083392a4b4fd6fa2f5
pauloazedo <paulo.azedo@gmail.com>
2025-04-16 16:47:10 -0500

    Fix basePath from next app, and adjusted Jenkinsfile.prod to improve Slack messages

--- my-portfolio/frontend/next.config.ts
+++ my-portfolio/frontend/next.config.ts
@@ -5,9 +5,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  // Dynamically set basePath based on env
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-
   async rewrites() {
     return [
       {
